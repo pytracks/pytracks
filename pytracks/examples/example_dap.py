c0830a0514dbf592fa082f76ab5e8f91bfee165c
alexandrejaguar <afdesiqueira@gmail.com>
2019-01-09 17:31:42 -0200

    Adding helping_window.

--- pytracks/examples/example_dap.py
+++ pytracks/examples/example_dap.py
@@ -1,6 +1,31 @@
+import tkinter as tk
 
-
+from tkinter import messagebox
 
 
 def example_dap():
+    """
+    """
+
+    helping_window('Step 1', 'Binarizing the input images.')
+
     return None
+
+
+def helping_window(title='default', message='default'):
+    """
+    """
+
+    window = tk.Tk()
+    window.withdraw()
+
+    messagebox.showinfo(title=title,
+                        message=message)
+    window.destroy()
+
+
+    return None
+
+
+example_dap()
+
